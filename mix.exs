db881943cbd4ce154ee1d223e7ff076171d0686a
x <kemo@sent.com>
2025-09-05 10:28:45 -0500

    try downgrading to 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FishySync.MixProject do
     [
       app: :fishy_sync,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
